4a67f375f6580cfc6875fd29a246e0db97c7ebd6
Dave Mitchell <david.mitchell@echostar.com>
2014-03-21 09:15:25 -0600

    fix receiver spec

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,11 +1,11 @@
 class Receiver
   def self.update(uuid, receiver_id, receiver_json)
-    $redis.set("r_#{receiver_id}", receiver_json)
-    #$redis.sadd("ur_#{uuid}", receiver_id)
+    $redis.set("rec_#{uuid}_#{receiver_id}", receiver_json)
+    #$redis.sadd("recu_#{uuid}", receiver_id)
   end
 
   def self.get(uuid, receiver_id)
-    $redis.get("r_#{receiver_id}")
+    $redis.get("rec_#{uuid}_#{receiver_id}")
   end
 
 end
--- spec/models/receiver_spec.rb
+++ spec/models/receiver_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Receiver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  after :each do
+    $redis.del($redis.keys('rec*')) unless $redis.keys('rec*').count == 0
+  end
+
+  it 'should create a key for the receiver' do
+    Receiver.update('BAKDS', 'R123908', '{"bogus":"json"}')
+    $redis.get('rec_BAKDS_R123908').should == '{"bogus":"json"}'
+  end
+
+  it 'should return the json for the uuid' do
+    Receiver.update('BAKDS', 'R123908', '{"bogus":"json"}')
+    Receiver.get('BAKDS', 'R123908').should == '{"bogus":"json"}'
+  end
 end
+
+
